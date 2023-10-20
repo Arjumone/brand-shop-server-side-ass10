8f4daf2b70778f38266677100fabd69e7869d376
Arju Mone <121635205+Arjumone@users.noreply.github.com>
2023-10-21 00:08:59 +0600

    get the all cart from database

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+// const corsOptions ={
+//   origin:'*', 
+//   credentials:true,
+//   optionSuccessStatus:200,
+// }
+
+// app.use(cors(corsOptions))
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1mhzzn6.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -26,7 +34,7 @@ async function run() {
     // await client.connect();
 
     const productCollection = client.db('productDB').collection('products')
-    const cartCollection = client.db('productDb').collection('carts')
+    const cartCollection = client.db('productDB').collection('carts')
 
     app.get('/products',async(req,res)=>{
       const cursor = productCollection.find()
@@ -49,18 +57,24 @@ async function run() {
     })
 
     app.post('/products',async(req,res)=>{
-      const newProducts = req.body 
+      const newProducts = req.body;
       const result = await productCollection.insertOne(newProducts)
       res.send(result);
   })
-    app.post('/cart',async(req,res)=>{
-      const newCart = req.body 
-      const result = await productCollection.insertOne(newCart)
+
+  app.get('/carts',async(req,res)=>{
+    const cursor = cartCollection.find()
+    const result = await cursor.toArray()
+    res.send(result)
+  })
+    app.post('/carts',async(req,res)=>{
+      const newCart = req.body ;
+      const result = await cartCollection.insertOne(newCart)
       res.send(result);
   })
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
